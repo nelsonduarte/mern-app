0009aaef36af5edfcf0c59be02fdc14ae1592a82
Nelson Duarte <nelson.duarte@islasantarem.pt>
2022-07-08 18:55:36 +0100

    .env deleted

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,24 +38,40 @@ const createUser = async (req, res) => {
     })
     res.status(200).json(user)
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: err.message })
   }
 }
 
 // Apagar um utilizador
+const deleteUser = async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await User.findOneAndDelete(_id)
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res
+        .status(404)
+        .json({ message: "Não foi possivel apagar o Utilizador" })
+    }
+    res.json(user)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
 
 // Atualizar um utilizador
 const updateUser = async (req, res) => {
   const { id } = req.params
   const { numero, nome, email, password } = req.body
   try {
-    const user = await User.findByIdAndUpdate(id)
+    const user = await User.findByIdAndUpdate(_id)
     if (!user) {
-      return res.status(404).json({ message: "Utilizador não encontrado" })
+      return res
+        .status(404)
+        .json({ message: "Não foi possivel atualizar o Utilizador" })
     }
     res.json(user)
   } catch (error) {
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: err.message })
   }
 }
 
@@ -64,5 +80,6 @@ module.exports = {
   createUser,
   getUsers,
   getUser,
+  deleteUser,
   updateUser,
 }
